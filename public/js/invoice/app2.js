19f75c9ef28f068d1bdb713119e8807e01112668
t_x_duyet <t_x_duyet@airily.co.jp>
2023-06-23 13:12:59 +0900

    2

--- public/js/invoice/app2.js
+++ public/js/invoice/app2.js
@@ -1,103 +1,98 @@
-import * as input from '@grapecity/wijmo.input';
-import * as wjFlexSheet from '@grapecity/wijmo.grid.sheet';
-//
+
 document.readyState === 'complete' ? init() : window.onload = init;
-//
 function init() {
-    wijmo.setLicenseKey("477492474881697#B0XzzWYmpjIyNHZisnOiwmbBJye0ICRiwiI34TUuJFMStWbqFzZ8MGbHx6NhZ5ROlTdolGen9GUvx4KBRXNPVUW5Y5SaplcG3UbQVTOYp6atB5L8MzKodjQ7ImQINzRylVW4IWTlxWR0BVOORXRQRjMHNXO6l5K9BTd92EOrITTGt6K8AVZNtkdvlTYY3CTEpEdsVTc6QXQERVd7R7diR5Y6VmcjB7TJJzUSFESvIDOldEZyE4ZFpkZolWT6lFWZN5SGVVUy2mUOB5cyVXMBtCd8dzUXJkcW9kUrQkQFlkQ6cHMGR7NpF7boJWd5tkZwIWWDF4ZTl7b4ckbqRFe6llNBJWd5FzcBNUdsVVZRRTO584KYh7cnBFaUlncQd6SUxGRk9ka7cmepVESaF4KtZ5Q6gVc48kYHxWekJ7bmZTUzN4ZRhkR7Y4TKdWWOlFePZmMMdDcpVkdxNlcvQ6KNBjRGlVOstCeoR5ZhNVOYNmTQdkI0IyUiwiIyQURygTRwEjI0ICSiwyNyYDN8kDNxcTM0IicfJye&Qf35VfikEMyIlI0IyQiwiIu3Waz9WZ4hXRgACdlVGaThXZsZEIv5mapdlI0IiTisHL3JSNJ9UUiojIDJCLi86bpNnblRHeFBCIyV6dllmV4J7bwVmUg2Wbql6ViojIOJyes4nILdDOIJiOiMkIsIibvl6cuVGd8VEIgc7bSlGdsVXTg2Wbql6ViojIOJyes4nI4YkNEJiOiMkIsIibvl6cuVGd8VEIgAVQM3EIg2Wbql6ViojIOJyes4nIzMEMCJiOiMkIsISZy36Qg2Wbql6ViojIOJyes4nIVhzNBJiOiMkIsIibvl6cuVGd8VEIgQnchh6QsFWaj9WYulmRg2Wbql6ViojIOJyebpjIkJHUiwiI4IjNwcDMgATM5AzMyAjMiojI4J7QiwiIx8CMuAjL7ITMiojIz5GRiwiI+S09ayL9Pyb9qCq9jK88GO887K88XO882O88sO88wK88iojIh94QiwiI7kjNxgDO4cDNykDN7cDNiojIklkIs4XXbpjInxmZiwiIyY7MyAjMiojIyVmWiDS");
-
-
-    let flex = new wjFlexSheet.FlexSheet('#unboundSheet');
-    flex.addUnboundSheet('unbound', list.length, headerkey.length);
-    flex.itemsSource = list;
-    console.info(flex);
-    flex.frozenColumns = 5; //   freeze column
-    flex.isReadOnly = true;
-    flex.isTabHolderVisible = false; // not show sheet name
-    //   flex.columnHeaders.columns.defaultSize = 50; // header width size
-    flex.columnHeaders.rows.defaultSize = 50; // header height size
-    flex.showFilterIcons = false; // hide filter icon
-    let setHeaderName = false,
-        colHeader, key,
-        width, ht, sourceRow, fmt;
-    flex.topLeftCells.setCellData(0, 0, "行No");
-    flex.topLeftCells.columns.defaultSize = 40;
-    flex.topLeftCells.hostElement.lastChild.lastChild.wordWrap = true;
-    for (let row = 0; row < list.length; row++) {
-        for (let col = 0; col < headerkey.length; col++) {
-            colHeader = flex.columnHeaders.columns[col];
-            key = headerkey[col];
-            if (!setHeaderName) {
-                // change header name from A, B , C....
-                colHeader.header = headername[key]["name"];
-                colHeader.wordWrap = true;
-                // colHeader.align = "center";
-
-                if (headername[key]["class"])
-                    colHeader.cssClass = headername[key]["class"];
-
-                width = parseInt(headername[key]["width"]);
-                if (width) {
-                    colHeader.width = width;
-                }
-            }
-            // set cell data
-            if (key != "Quantity") {
-                fmt = "n0";
-            } else {
-                fmt = "n2";
-            }
-            // set cell number format
-            flex.setCellData(row, col, wijmo.Globalize.format(list[row][key], fmt));
-        }
-        setHeaderName = true;
+    var headerCol = [], datacopy = null;
+    // set header name, width, css
+    for (let col = 0; col < headerkey.length; col++) {
+        let key = headerkey[col];
+        headerCol.push({
+            binding: headerkey[col],
+            header: headername[key]["name"],
+            width: headername[key]["width"],
+            wordWrap: true,
+            cssClass: headername[key]["class"]
+        })
     }
+    // create Grid table
+    var flex = new wijmo.grid.FlexGrid("#grid", {
+        itemsSource: new wijmo.collections.ObservableArray(list),
+        columns: headerCol,
+        frozenColumns: 5,
+        isReadOnly: true,
+        allowResizing: 'BothAllCells',
+        selectionMode: 'Row'
+    })
+    // set css style 
+    flex.topLeftCells.setCellData(0, 0, "行No");
+    flex.columnHeaders.rows[0].cssClass = "wj-align-center"
+    flex.columnHeaders.rows[0].wordWrap = true;
+    flex.columnHeaders.rows.defaultSize = 55;
+    flex.rows.defaultSize = 59;
+    setRowHeaderNumber(flex)
+
     flex.hostElement.addEventListener('contextmenu', (e) => {
         ht = flex.hitTest(e);
-        console.info(menu)
-
-        if (ht.cellType == 1) {
+        // set select rows style
+        flex.selection = new wijmo.grid.CellRange(ht.row, 0, 1, 1)
+        if (ht.cellType == 2) {
+            // show on row header only
             e.preventDefault();
             e.stopImmediatePropagation();
         }
     }, true);
-    let menu = new input.Menu(document.createElement('div'), {
+    let menu = new wijmo.input.Menu(document.createElement('div'), {
         displayMemberPath: 'header',
         selectedValuePath: 'cmd',
         dropDownCssClass: 'ctx-menu',
         itemsSource: [
-            { header: '<span class="glyphicon glyphicon-plus"></span>&nbsp;&nbsp;挿入', cmd: 'cmdNew' },
-            { header: '<span class="glyphicon glyphicon-copy"></span>&nbsp;&nbsp;コピー', cmd: 'cmdCopy' },
-            { header: '<span class="glyphicon glyphicon-paste"></span>&nbsp;&nbsp;貼り付け', cmd: 'cmdPaste' },
-            { header: '<span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;コピーした行の挿入', cmd: 'cmdPasteNew' },
-            { header: '<span class="glyphicon glyphicon-trash"></span>&nbsp;&nbsp;削除', cmd: 'cmdDel' },
+            { header: '<span class="glyphicon glyphicon-plus"></span>挿入', cmd: 'cmdNew' },
+            { header: '<span class="glyphicon glyphicon-copy"></span>コピー', cmd: 'cmdCopy' },
+            { header: '<span class="glyphicon glyphicon-paste"></span>貼り付け', cmd: 'cmdPaste' },
+            { header: '<span class="glyphicon glyphicon-share"></span>コピーした行の挿入', cmd: 'cmdPasteNew' },
+            { header: '<span class="glyphicon glyphicon-trash"></span>削除', cmd: 'cmdDel' },
             { header: '<span class="wj-separator"></span>' },
-            { header: '<span class="glyphicon glyphicon-remove"></span>&nbsp;&nbsp;閉じる', cmd: 'cmdExit' }
+            { header: '<span class="glyphicon glyphicon-remove"></span>閉じる', cmd: 'cmdExit' }
         ],
         itemClicked: () => {
-            console.info(ht)
             // 挿入
             if (menu.selectedValue == "cmdNew") {
-                flex.insertRows(ht.row, 1)
+                flex.itemsSource.insert(ht.row, null);
             }
             // 削除
             if (menu.selectedValue == "cmdDel") {
-                flex.rows.removeAt(ht.row)
+                $("#ConfirmModal").modal("show")
+                // flex.itemsSource.removeAt(ht.row);
             }
             // コピー
             if (menu.selectedValue == "cmdCopy") {
-                sourceRow = flex.rows[ht.row];
+                datacopy = flex.itemsSource[ht.row];
             }
             // 貼り付け
             if (menu.selectedValue == "cmdPaste") {
-                setCopyData(flex, ht, sourceRow)
+                setCopyData(datacopy, ht)
             }
             // コピーした行の挿入
             if (menu.selectedValue == "cmdPasteNew") {
-                flex.insertRows(ht.row, 1)
-                setCopyData(flex, ht, sourceRow)
+                if (datacopy) {
+                    flex.itemsSource.insert(ht.row, null);
+                    setCopyData(datacopy, ht)
+                }
             }
+            setRowHeaderNumber(flex);
+            setRowSelected(flex, ht)
         }
+    });
+    // disable menu when not have data copy
+    menu.isDroppedDownChanging.addHandler((s, e) => {
+        s.dropDown.childNodes.forEach(item => {
+            if (!datacopy) {
+                if (item.innerText == '貼り付け' || item.innerText == 'コピーした行の挿入') {
+                    wijmo.addClass(item, 'wj-state-disabled');
+                }
+            } else {
+                wijmo.removeClass(item, 'wj-state-disabled');
+            }
+        });
     });
     // use it as a context menu for one or more elements
     let els = document.querySelectorAll('.has-ctx-menu');
@@ -110,13 +105,22 @@ function init() {
             }
         }, true);
     }
-    function setCopyData(flex, ht, sourceRow) {
-        if (sourceRow) {
-            for (let i = 0; i < flex.columns.length; i++) {
-                //copy data,optionally update formula
-                flex.setCellData(ht.row, i, flex.getCellData(sourceRow.index, i, false), false, true);
-            }
-            sourceRow = null;
+
+    // set data copy
+    function setCopyData(datacopy, ht) {
+        if (datacopy) {
+            flex.itemsSource[ht.row] = datacopy;
+            flex.collectionView.refresh()
         }
+    }
+    // set number line
+    function setRowHeaderNumber(flex) {
+        for (var r = 0; r < flex.rowHeaders.rows.length; r++) {
+            flex.rowHeaders.setCellData(r, 0, r + 1);
+        }
+    }
+    // set selected row style
+    function setRowSelected(flex, ht) {
+        flex.selection = new wijmo.grid.CellRange(ht.row, 0, 1, 1)
     }
 }
