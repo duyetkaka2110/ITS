5896de05ff48b4d4f2022633e08036f8330c62a4
t_x_duyet <t_x_duyet@airily.co.jp>
2023-08-08 10:56:25 +0900

    a

--- resources/views/mitsumori/index2.blade.php
+++ resources/views/mitsumori/index2.blade.php
@@ -4,6 +4,8 @@
 <link href="{{ URL::asset('css/jquery-ui.css') }}" rel="stylesheet" />
 <link href="https://unpkg.com/tabulator-tables@5.5.0/dist/css/tabulator.min.css" rel="stylesheet">
 <link href="{{ URL::asset('css/mitsumori.css') }}" rel="stylesheet" />
+<link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/fixedheader/3.4.0/css/fixedHeader.dataTables.min.css" rel="stylesheet" />
 <style>
     .container-fluid {
         max-width: 2133px;
@@ -71,20 +73,24 @@
     .tabulator-row.tabulator-selected .text-danger {
         color: #fff !important;
     }
-    #container{
+
+    #container {
         height: 500px;
         overflow: scroll;
     }
-    .table-custom{
+
+    .table-custom {
         height: 500px;
         overflow: scroll;
     }
 </style>
 @endsection
 @section("js")
-<script src="{{ URL::asset('js/js.cookie.min.js') }}"></script>
-<script src="{{ URL::asset('js/jquery-ui.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/js.cookie.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/jquery-ui.min.js') }}"></script>
 <script type="text/javascript" src="https://unpkg.com/tabulator-tables@5.5.0/dist/js/tabulator.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/fixedheader/3.4.0/js/dataTables.fixedHeader.min.js"></script>
 
 <script>
     var list = <?php echo $list ?>;
@@ -96,9 +102,41 @@
     var headerZairyoSelected = <?php echo $headerZairyoSelected ?>;
 </script>
 
+<style>
+
+.dataTables_info{
+    display: none;
+}
+    </style>
 <script>
     $(document).ready(function() {
-        convert()
+        // Get the keys (column names) of the first object in the JSON data
+        console.info(list)
+        let cols = [];
+        $.each(list[0], function(key, val) {
+            cols.push({
+                title: key,
+                data: key
+            })
+        })
+
+        new DataTable('#example', {
+            columns: cols,
+            data: list,
+            fixedHeader: true,
+            paging: false,
+            searching: false,
+            select: {
+                style: "single",
+                className: "row-selected"
+            },
+            responsive: true,
+            searchPanes: {
+                controls: false
+            },
+            dom: 'Plfrtip'
+        });
+        $('.dataTables_info:eq(1)').hide();
         // Function to convert JSON data to HTML table
         function convert() {
 
@@ -106,7 +144,7 @@
             let jsonData = list;
 
             // Get the container element where the table will be inserted
-            let container = $("#container");
+            let container = $("#table");
 
             // Create the table element
             let table = $("<table class='table table-striped table-bordered table-custom'>");
@@ -242,8 +280,11 @@
 
 @endsection
 @section('content')
+<div id="container" class="display">
+    <table id="example" class="display" style="width:100%">
+    </table>
+</div>
 <div class="page"></div>
-<div id="container"></div>
 <div class="container-fluid">
     <div class="mg-menu"></div>
     <div id="grid" class="not-"></div>
